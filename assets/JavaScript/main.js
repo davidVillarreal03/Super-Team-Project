f3f1f080151442a64a5944d2a3e46952e39f089a
David Villarreal <djmarch92014@gmail.com>
2024-06-25 20:24:26 -0500

    I have added a function into the js to get the history of the search engine and update it into the local storage for the use to see what the last thing they looked up was.

--- assets/JavaScript/main.js
+++ assets/JavaScript/main.js
@@ -1,9 +1,21 @@
 const hotBtn = document.querySelector("#hot");
 const topBtn = document.querySelector("#top");
 const newBtn = document.querySelector("#new");
+const historyBtn = document.querySelector("#history");
+const history = $('#search-engine');
+
+function saveHistoryToStorage(history) {
+  const historyStorage = history.val().trim();
+  localStorage.setItem('history', JSON.stringify(historyStorage));
+}
+
+function history() {
+  let read = JSON.parse(localStorage.getItem('history'));
+  console.log(read);
+}
 
 const accessToken =
-  "eyJhbGciOiJSUzI1NiIsImtpZCI6IlNIQTI1NjpzS3dsMnlsV0VtMjVmcXhwTU40cWY4MXE2OWFFdWFyMnpLMUdhVGxjdWNZIiwidHlwIjoiSldUIn0.eyJzdWIiOiJ1c2VyIiwiZXhwIjoxNzE5MzQ2NTkzLjYxNTM5LCJpYXQiOjE3MTkyNjAxOTMuNjE1MzksImp0aSI6InZBNzhrVHo1bnQ5dHpvRlFGZm5fZXZUT0lGOVI2dyIsImNpZCI6IkJHcU5SbGNlNjNmb3NrSi1PQVkwckEiLCJsaWQiOiJ0Ml8xMnMxMHlmMzljIiwiYWlkIjoidDJfMTJzMTB5ZjM5YyIsImxjYSI6MTcxODc1NzgyNjU0OSwic2NwIjoiZUp5S1Z0SlNpZ1VFQUFEX193TnpBU2MiLCJmbG8iOjl9.RBRpCE9UHAZvhCouGAd23jinCOhJF2cmttEFTUShcpCPp8zORLXS0cSSo13a4med3IX7odtidYYNLJ5tUwpycE17C6I2gB6nqtz2X9nb_NvT_ad1ZZ0vcl1CdhtKn-L-tnQ8w_IQWNRB1zrjlZhw56ZoEFWigUMtktGADoXK_PWLnJ13P1eDzhndvKDzjlA24T75b7o7gK-SASA1G8boJXy0lntzFozXg-x5dSjGyhfHC_gin2wa0JXCgaOpXVd79E8ETDUdVIUiJINd8nLBpkfCcqk8Xgc-XISuSFmkRpOF8OaJRpSxFak2efJp29L-n7eUiP4NzFvRTYqQUz9Q_Q";
+"eyJhbGciOiJSUzI1NiIsImtpZCI6IlNIQTI1NjpzS3dsMnlsV0VtMjVmcXhwTU40cWY4MXE2OWFFdWFyMnpLMUdhVGxjdWNZIiwidHlwIjoiSldUIn0.eyJzdWIiOiJ1c2VyIiwiZXhwIjoxNzE5NDQ1MzU5LjYzNjUzMywiaWF0IjoxNzE5MzU4OTU5LjYzNjUzMywianRpIjoiRnQ4U3hGU2x5ajZhVmtKWHdXcjQ4Z0FPVjZJN3FBIiwiY2lkIjoiMl9XVEZHcmtHWEhzZnpVdW9yam5QZyIsImxpZCI6InQyX3R0dW5hcG1pOCIsImFpZCI6InQyX3R0dW5hcG1pOCIsImxjYSI6MTcwNzQ3MTQ2MjM5Miwic2NwIjoiZUp5S1Z0SlNpZ1VFQUFEX193TnpBU2MiLCJmbG8iOjl9.ZJ3wCka2czjsW1ISB7bUQo-c9K08qyN53MLYWCOD4WFmCsKEzVb_qOCvG5IkDErqxV5V82cAzZHT2PqN8TZNhkdF9yyC2rnbKFBVyxTcu_nvkpzX6PD2TKq0pHhuf5i421BsweO5UNGVeipeN_IWEHcFYg-lOlHdu9TXHcRynsyxyccg53Vg5Y26hCj9i67TUD6bhOo4rZeUx8TSqPWYqmFW6CxKeWXhLSxSpUlP71JJx3Nnepse7UNRi8lKPOIzlCG7YtLnbs6WwaGMtyTqwMvGbYt63tkbgndhhJ3HjWnOwR-GJrhzXE9A3lJh_c0xHBIdl_F7UThszkXm-PwRVQ";
 function SortNewApi() {
   fetch("https://oauth.reddit.com/r/sanantonio/new.json?limit=5", {
     headers: {
@@ -17,7 +29,6 @@ function SortNewApi() {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
       for(let i = 0; i < 5; i++) {
         console.log(data.data.children[i].data.title);
         console.log(data.data.children[i].data.link_flair_text);
@@ -39,7 +50,6 @@ function SortTopApi() {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
       for(let i = 0; i < 5; i++) {
         console.log(data.data.children[i].data.title);
         console.log(data.data.children[i].data.link_flair_text);
@@ -61,7 +71,6 @@ function SortHotApi() {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
       for(let i = 0; i < 5; i++) {
         console.log(data.data.children[i].data.title);
         console.log(data.data.children[i].data.link_flair_text);
@@ -95,3 +104,4 @@ function SortHotApi() {
 newBtn.addEventListener("click", SortNewApi);
 hotBtn.addEventListener("click", SortHotApi);
 topBtn.addEventListener("click", SortTopApi);
+historyBtn.addEventListener("click", history);
